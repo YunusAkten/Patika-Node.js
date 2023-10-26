23d45e0c290099a9a80b3cad1b906a8115bda5af
YunusAkten <yunusakten2@gmail.com>
2023-10-26 15:39:56 +0300

    CleanBlog-2 ejs

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const blog= { id: 1, title: "Blog title", description: "Blog description" };
+const path = require('path');
+
+// const blog= { id: 1, title: "Blog title", description: "Blog description" };
+app.use(express.static('public'));
+app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
-    res.status(200).send(blog);
-    res.end();
+    res.render("index")
+});
+app.get('/about', (req, res) => {
+  res.render("about")
+});app.get('/add_post', (req, res) => {
+  res.render("add_post")
 });
 app.listen(port, () => {
   console.log('Server is running on port '+port);
